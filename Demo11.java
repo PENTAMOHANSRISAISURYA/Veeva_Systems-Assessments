89a910afcfef528727a68e6f47084d9039a81dc7
PENTA MOHAN SRI SAI SURYA <pmsssurya2005@gmail.com>
2026-01-21 11:43:19 +0530

    Update Demo11.java

--- Demo11.java
+++ Demo11.java
@@ -2,23 +2,20 @@ class InvalidPasswordException extends Exception {
 }
 
 class Demo11{
-
     void validatePassword(String password) throws InvalidPasswordException {
         if (password.length() < 8) {
             throw new InvalidPasswordException();
         }
     }
-
     public static void main(String[] args) {
-
         String password = "abc123";  
-	PasswordValidator p = new PasswordValidator();
-
+	    Demo11 d = new Demo11();
         try {
-            p.validatePassword(password);
+            d.validatePassword(password);
         } catch (InvalidPasswordException e) {
             System.out.println("Invalid Password.");
         }
     }
 }
+
 
