088918b5b4135435428caf09fab7de3b1f69b368
PENTA MOHAN SRI SAI SURYA <pmsssurya2005@gmail.com>
2026-01-21 12:21:33 +0530

    Rename Demo13.java to Demo_14.java

--- Demo_14.java
+++ Demo_14.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.stream.*;
 
-public class Demo13 {
+public class Demo_14{
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
@@ -9,3 +9,4 @@ public class Demo13 {
         System.out.println(evenNumbers);
     }
 }
+
